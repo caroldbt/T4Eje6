293806f90c957d3babc4523328b9cc128e5ccfd9
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-09 23:54:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,56 @@ namespace T4Eje6
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /************
+             * Ejercicio 6
+             * **********/
+            double precio=0;
+            bool entrada = false;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Indique precio del producto: ");
+                    precio = Convert.ToDouble(Console.ReadLine());
+                    entrada = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Debe ingresar un valor numerico.");
+                }
+            } while (!entrada);
+
+
+            if (entrada)
+            {
+                if (precio > 0)
+                {
+
+                    string cuenta;
+                    bool opcion = false;
+                    do
+                    {
+                        Console.WriteLine("Indique forma de pago, tarjeta/efectivo:");
+                        string texto = Console.ReadLine().ToLower();
+                        switch (texto)
+                        {
+                            case "tarjeta":
+                                Console.WriteLine("Indique numero de cuenta.");
+                                cuenta = Console.ReadLine();
+                                Console.WriteLine("Compra realizada con exito.");
+                                opcion = true; break;
+                            case "efectivo":
+                                Console.WriteLine("Gracias por su compra.");
+                                opcion = true; break;
+                            default:
+                                Console.WriteLine("Opcion incorrecta, indique tarjeta o efectivo");
+                                opcion = false;
+                                break;
+                        }
+
+                    } while (!opcion);
+                }
+            }
         }
     }
 }
